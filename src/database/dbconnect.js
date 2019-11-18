61bcd308976673b2070cf64061b62e72d160487e
Hammed Taofeek <htolajide@yahoo.com>
2019-11-18 12:49:39 +0100

    Update dbconnect.js
    Connection update

--- src/database/dbconnect.js
+++ src/database/dbconnect.js
@@ -10,11 +10,12 @@ const env = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 console.log('this is the environment: ', env );
 
 if (env === 'production') {
-const pool = pg.Pool({ connectionString: process.env.DATABASE_URL,});
-pool.on('connect', () => {
-  debug('app:database')('connected to the Database');
-  module.exports = pool;
-});
+  pg.defaults.ssl = true;
+  const pool = pg.Pool({ connectionString: process.env.DATABASE_URL,});
+  pool.on('connect', () => {
+    debug('app:database')('connected to the Database');
+    module.exports = pool;
+  });
 } else if (env === 'test') {
   const pool = pg.Pool(configuration.test);
   pool.on('connect', () => {
