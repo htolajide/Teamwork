158ec66be72f12507da665286e5f526c13288567
Hammed Taofeek <htolajide@yahoo.com>
2019-11-20 18:27:04 +0100

    Update app.js
    App updated

--- src/app.js
+++ src/app.js
@@ -15,12 +15,6 @@ dotenv.config();
 const app = express();
 
 // to resolve cross origin resource shearing (CORS) error add folowing to te response header 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
